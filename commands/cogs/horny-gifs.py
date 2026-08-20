ec18748d7ba2d485b210749e3ceee8d65f4d7f05
Clove Twilight <admin@doughmination.win>
2026-08-20 21:27:45 +0100

    aaa

---
+++ commands/cogs/horny-gifs.py
@@ -0,0 +1,54 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+gifUrls = {
+"Catgirl Sucking": "catgirl-suck",
+"Cum Filled": "cum-filled",
+"Cum Thirsty": "cum-thirsty",
+"Deep Kiss": "deep-kiss",
+"Double Blowjob": "double-suck",
+"Held Up Anal": "held-up-anal",
+"Pregnant Pillow": "pregnant-pillow",
+"Shuddup": "shuddup",
+"Shut the fuck up": "stfu",
+"Stop Yapping": "stop-yapping",
+"Yeah Yeah Whatever": "whatever"
+}
+
+class Gifs(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    async def gifAuto(
+        self, interaction: discord.Interaction, current: str
+    ) -> list[app_commands.Choice[str]]:
+        result = []
+        for name in gifUrls:
+            if current.lower() in name.lower():
+                result.append(app_commands.Choice(name=name, value=name))
+        return result[:25]
+
+    @app_commands.command(name="nsfw-gif", description="Moans!", nsfw=True)
+    @app_commands.describe(gif="The gif to grab")
+    @app_commands.autocomplete(gif=gifAuto)
+    async def gifSlash(self, interaction: discord.Interaction, gif: str) -> None:
+        channel = interaction.channel
+        if not isinstance(channel, (discord.TextChannel, discord.Thread)) or not channel.is_nsfw():
+            await interaction.response.send_message(
+                "This command can only be used in an age-restricted (NSFW) channel.",
+                ephemeral=True,
+            )
+            return
+        await interaction.response.defer()
+        reply = await self.getGifUrl(gif)
+        await interaction.followup.send(embed=reply)
+    
+    async def getGifUrl(self, gif: str) -> discord.Embed:
+        embed = discord.Embed(color=discord.Color.fuchsia())
+        slug = gifUrls.get(gif)
+        embed.set_image(url=f"https://m.doughmination.gay/gif/nsfw/{slug}.gif")
+        return embed
+    
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Gifs(bot))
